11d2a85bf5762e26f04ddd3581cd6915e9d48c6b
Andrew Parys <andrew_parys@live.com>
2018-08-15 15:34:33 +0300

    Small changes for auth for lite.

---
+++ MyLiverpool.Web.Mvc/Controllers/CommentController.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyLfc.Common.Web;
+using MyLfc.Common.Web.DistributedCache;
+using MyLiverpool.Business.Contracts;
+using MyLiverpool.Business.Dto;
+using MyLiverpool.Common.Utilities.Extensions;
+
+namespace MyLiverpool.Web.Mvc.Controllers
+{
+    [Authorize]
+    [Route("[controller]/[action]")]
+    public class CommentController : Controller
+    {
+        private readonly ICommentService _commentService;
+        private readonly IDistributedCacheManager _cacheManager;
+
+        public CommentController(ICommentService commentService, IDistributedCacheManager cacheManager)
+        {
+            _commentService = commentService;
+            _cacheManager = cacheManager;
+        }
+        
+        [Authorize, HttpPost]
+        public async Task<IActionResult> Create([FromBody] CommentDto dto)
+        {
+            dto.IsVerified = false;//IsSiteTeamMember();
+            dto.AuthorId = User.GetUserId();
+            var result = await _commentService.AddAsync(dto);
+
+            _cacheManager.Remove(CacheKeysConstants.MaterialList);
+            _cacheManager.Remove(CacheKeysConstants.LastComments);
+            result.AuthorUserName = User.Identity.Name;
+
+         //   var oldMessage = result.Message.Substring(0);
+         //   SanitizeComment(result);
+
+            if (!string.IsNullOrWhiteSpace(result.Message))
+            {
+      //todo          _signalRHubAggregator.Send(HubEndpointConstants.AddComment, result);
+            }
+
+         //  result.Message = oldMessage;
+
+            return Ok(result);
+        }
+    }
+}
--- MyLiverpool.Web.Mvc/wwwroot/js/site.js
+++ MyLiverpool.Web.Mvc/wwwroot/js/site.js
@@ -20,7 +20,7 @@
         }
     });
 
-    $(".short-link").each(function() {
+    $(".short-link").each(function () {
         const fullLink = new URL($(this).attr("href")).hostname;
         $(this).text(fullLink);
     });
@@ -34,11 +34,92 @@
             document.getElementById("goToTop").className = "hidden";
     };
 
-    window.onload = function() {
+    window.onload = function () {
         if (location.hash) {
             if (location.hash.indexOf("com")) {
                 $(location.hash).addClass("active");
             }
         }
     };
+
+    function getPlugins(type) {
+        const common = `autolink image paste customEmoticons`;
+        const type1 = `advlist lists link hr media textcolor colorpicker ${common}`;
+        if (type === 1) {
+            return type1;
+        }
+        if (type === 2) {
+            return `code fullscreen table visualblocks ${type1}`;
+        }
+        if (type === 3) {
+            return `${common}`;
+        }
+        return "";
+    }
+
+    
+    function getToolbar(type) {
+        const common =
+            `bold italic underline strikethrough | customEmoticons`;//poiler-add spoiler-remove`;
+        const type1 = `styleselect | ${common} | link image media | fontsizeselect hr
+                                 | bullist numlist | forecolor backcolor | alignleft aligncenter alignright alignjustify | outdent indent`;
+        const type2 = `undo redo | fullscreen ${type1} | table code | visualblocks`;
+        if(type === 1) {
+            return type1;
+        }
+        if (type === 2) {
+            return type2;
+        }
+        if (type === 3) {
+            return common;
+        }
+        return "";
+    }
+
+    function initTiny(elementId, type = 1) {
+        const elemId = "#" + elementId;
+        const settings1
+            = {
+                autoresize_overflow_padding: 0,
+                selector: elemId,
+                convert_urls: true,
+                schema: "html5",
+                fontsize_formats: "8pt 10pt 11pt 12pt 14pt 16pt",
+                forced_root_block: "",
+                min_height: this.height,
+                browser_spellcheck: true,
+                gecko_spellcheck: true,
+                remove_trailing_brs: true,
+                menubar: false,
+                language: "ru",
+                // inline: true,
+                plugins: [
+                     getPlugins(type)
+                ],
+                allow_script_urls: true,
+                relative_urls: true,
+                document_base_url: "/",
+                toolbar: getToolbar(type),
+                visualblocks_default_state: true,
+                external_plugins: {
+                    customEmoticons: "/plugins/customEmoticons/plugin.js"
+                },
+                skin_url: "/src/lightgray",
+                setup: (editor) => {//Editor) => {
+                    this.editor = editor;
+                    editor.on("change", () => {
+                        const content = editor.getContent();
+                        $(elemId).html(content);
+                    });
+                    editor.on("keyup", () => {
+                        const content = editor.getContent();
+                        $(elemId).html(content);
+                    });
+                }
+            }
+        tinymce.init(settings1);
+    }
+
+    initTiny("newComment");
 })
+
